ca71e8ebb724b6c8c44c400ca70b962f500040c9
github username <dreedsanders@gmail.com>
2021-03-01 08:43:51 -0600

    commit tree a3d544082ee0b2d460b504e375c68c4f819e7fbe

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,36 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all<<self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Paintings.all.select do |painting|
+      painting.artist == self
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+
+
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -7,4 +7,22 @@ class Gallery
     @city = city
   end
 
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  def artists
+    paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+  def artist_names
+    artists.map do |artist|
+      artist.name
+    end
+  end
+
 end
